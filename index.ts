08c42961f8674a9adb7d7f3513d6e56df7210eed
simdi <sim04ful@gmail.com>
2021-03-22 15:40:13 +0000

    removed user id

--- index.ts
+++ index.ts
@@ -2,14 +2,14 @@ import { Queue, Worker, Job, QueueScheduler } from 'bullmq';
 import * as redis from 'ioredis'
 
 
-// import { promisify } from 'util'
 
-const redisClient = redis('profilestwitter.nwr8vq.0001.use1.cache.amazonaws.com', { enableAutoPipelining: true })
+
+const redisClient = new redis('ec2-3-236-123-111.compute-1.amazonaws.com', { enableAutoPipelining: true })
 
 const schedulerQueue = new QueueScheduler('scheduler')
 
-const importQueue = new Queue('import');
-const exportQueue = new Queue('export')
+const importQueue = new Queue('import')
+const exportQueue = new Queue('export') //QUEUE OF MARSHALLED
 
 
 
@@ -17,22 +17,15 @@ function addToRedisSet(marshalledUsersInfo: MarshalledUserInfo[]): Promise<numbe
     return redisClient.sadd('twitterusers', marshalledUsersInfo)
 }
 
-function getNonExistingUsersInfo(marshalledUsersInfo: MarshalledUserInfo[]): Promise<UserName[]> {
-  
-    return new Promise((resolve, reject) => {
-        redisClient.smismember('twitterusers', marshalledUsersInfo, (err: Error, reply: number[]) => {
-
-            console.log(reply)
-            err ? reject(err) : resolve([])
-        })
-    })
+async function getNonExistingUsersInfo(marshalledUsersInfo: MarshalledUserInfo[]): Promise<MarshalledUserInfo[]> {
+    const result = await (redisClient as any).smismember('twitterusers', marshalledUsersInfo) as number[]
+    return marshalledUsersInfo.filter((_, i) => result[i] === 0)
 }
 
 type UserName = string //actually user handle
 type MarshalledUserInfo = string
 
 interface UserInfo {
-    id: string
     name: UserName
     followers: UserName[]
     photoUrl: string
@@ -49,7 +42,7 @@ async function addUserInfoExportJob(marshalledUsersInfo: string) {
 
 
 async function addInitialJob() {
-    await addUserImportJob('simdi_jinkins,twitterapi')
+    await addUserImportJob('simdi_jinkins')
 }
 
 
@@ -69,15 +62,13 @@ const importWorker = new Worker<UserName, void>('import', async (job) => {
 
     const followerUserNames = usersInfo.filter(({ name }) => nonExistingUsersNames.includes(name)).flatMap(u => u.followers)
 
-
-
     await addUserInfoExportJob(filteredMarshalledUsersInfo.join(','))
     await addUserImportJob(followerUserNames.join(','))
 
 }, { concurrency: 10 })
 
 const exportWorker = new Worker<string, void>('export', async (job) => {
-    await addToRedisSet(job.data.split(','))
+    await addToRedisSet(job.data.split(',')) //EXPORT USER INFO
 }, { concurrency: 1000 })
 
 
@@ -86,26 +77,13 @@ async function getUsersInfo(usernames: UserName[]): Promise<UserInfo[]> {
     return []
 }
 
-async function getFollowersUserName(userId: string): Promise<UserName[]> {
-    return []
-}
 
-async function getExistingUserNameCount(usernames: UserName[]): Promise<number> {
-    return 0
-}
-
-async function exportUserInfo(marshalledUsersInfo: MarshalledUserInfo[]): Promise<true> {
-
-    return true
-
-}
-
-function marshallUserInfo({ id, name, photoUrl, followers }: UserInfo): MarshalledUserInfo {
-    return [id, name, parsePhotoIdFromPhotoUrl(photoUrl), followers.length].join('\n')
+function marshallUserInfo({ name, photoUrl, followers }: UserInfo): MarshalledUserInfo {
+    return [name, parsePhotoIdFromPhotoUrl(photoUrl), followers.length].join('\n')
 }
 
 function getUserNameFromMarshalledUserInfo(marshalledUsersInfo: MarshalledUserInfo): string {
-    return marshalledUsersInfo.split('\n')[1]
+    return marshalledUsersInfo.split('\n')[0]
 }
 
 function parsePhotoIdFromPhotoUrl(photoUrl: string): string {
